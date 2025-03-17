3c82ca42a1e9c7f71077e70f3d26d3a46611ffac
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-03-17 21:09:30 +0100

    Update orientation.js

--- orientation.js
+++ orientation.js
@@ -11,13 +11,39 @@
  * @param {orientationCallback} onOrientationChange - Callback function handling orientation changes
  */
 function setupOrientationCapture(onOrientationChange) {
+  /**
+   * Handles device orientation events
+   * @param {DeviceOrientationEvent} event - The device orientation event object
+   */
+  function handleOrientationEvent(event) {
+    const rotateDegrees = event.alpha; // alpha: rotation around z-axis
+    const leftToRight = event.gamma; // gamma: left to right
+    const frontToBack = event.beta; // beta: front back motion
+    onOrientationChange(frontToBack, leftToRight, rotateDegrees);
+  }
+
   try {
-    window.addEventListener("deviceorientation", (event) => {
-      const rotateDegrees = event.alpha; // alpha: rotation around z-axis
-      const leftToRight = event.gamma; // gamma: left to right
-      const frontToBack = event.beta; // beta: front back motion
-      onOrientationChange(frontToBack, leftToRight, rotateDegrees);
-    });
+    // Handle iOS 13+ devices.
+    // @ts-ignore
+    if (typeof DeviceMotionEvent.requestPermission === "function") {
+      // @ts-ignore
+      DeviceMotionEvent.requestPermission()
+        .then((state) => {
+          if (state === "granted") {
+            window.addEventListener(
+              "deviceorientation",
+              handleOrientationEvent
+            );
+          } else {
+            console.error("Request to access the orientation was rejected");
+          }
+        })
+        .catch(console.error);
+    }
+    // Handle regular non iOS 13+ devices.
+    else {
+      window.addEventListener("deviceorientation", handleOrientationEvent);
+    }
   } catch (error) {
     console.error("Error setting up orientation capture:", error);
   }
