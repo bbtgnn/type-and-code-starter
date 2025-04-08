bfb1a8298cf69060cf744cf3c952b0e28f5e358e
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-04-08 11:22:59 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 export const configurazione = {
-  testo: "G",
+  testo: "a",
 
   dimensione: 0.8,
   interlinea: 0.7,
@@ -42,18 +42,10 @@ export function disegnaPunto({
   beta = 0,
   gamma = 0,
 }) {
-  const size = sin((frameCount + indice) * 6) * ((volume * unita) / 2) * unita;
-
-  if (indice % 2 == 0) {
-    fill("black");
-  } else {
-    fill("white");
-  }
-  noStroke();
-
+  // noStroke();
   push();
   translate(x, y);
-  ellipse(0, 0, size);
+  line(0, 0, alpha, gamma);
   pop();
 }
 
@@ -70,6 +62,7 @@ export function caricamentoRisorse() {}
 export function impostazioni() {
   frameRate(30);
   angleMode(DEGREES);
+  rectMode(CENTER);
 }
 
 /**
